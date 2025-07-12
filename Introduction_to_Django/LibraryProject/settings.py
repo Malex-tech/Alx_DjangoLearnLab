e12bb6ed2c9d34a91a7537c190d48d901ac6d1bb
Alex Alexander <raregemalex@gmail.com>
2025-07-12 19:28:45 +0100

    Modified Folder

--- Introduction_to_Django/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookshelf',
 
-
+    'LibraryProject.bookshelf',  # ✅ Correct app path now that it lives inside LibraryProject/
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
