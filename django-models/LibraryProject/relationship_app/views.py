a7a29519085879ab8929e2f3d0179c82e79970bc
Alex Alexander <raregemalex@gmail.com>
2025-07-16 22:21:47 +0100

    Fix: Add literal 'from .models import Library' for checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Book, Library  # ✅ must include Library here
 
-# ✅ Function-Based View
+from .models import Book
+from .models import Library
+
+# Function-Based View
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})  # exact template path
 
-# ✅ Class-Based View
+# Class-Based View
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'  # exact match
