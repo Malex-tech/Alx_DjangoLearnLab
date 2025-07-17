2188be85fbb3ba5bbd6674b8ce6313c21f8b5447
Alex Alexander <raregemalex@gmail.com>
2025-07-17 07:05:25 +0100

    Modified Project files

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('relationship_app.urls')),  # this line assumes 'relationship_app/urls.py' exists
     path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),
 ]
