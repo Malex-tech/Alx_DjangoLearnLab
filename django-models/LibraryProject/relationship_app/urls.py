22a111c3f5dc52f45e4bb2c5957a8e8c385077ab
Alex Alexander <raregemalex@gmail.com>
2025-07-19 23:22:11 +0100

    Modified django-models/LibraryProject/relationship_app/urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,5 @@
 # relationship_app/urls.py
+
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
@@ -8,7 +9,9 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
-    path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
+    
+    # Book routes
+    path('add_book/', views.add_book, name='add_book'),  # <-- THIS
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),  # <-- AND THIS
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 ]
