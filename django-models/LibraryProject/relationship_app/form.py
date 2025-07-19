e199f23761eda172740170a620cce682372efa0c
Alex Alexander <raregemalex@gmail.com>
2025-07-19 23:57:01 +0100

    Add Modified Project files

--- django-models/LibraryProject/relationship_app/form.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Book  # Replace with your actual model
-
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'published_date']  # Customize as needed
