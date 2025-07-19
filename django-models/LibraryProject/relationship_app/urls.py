7fa34162ee91258d52832e9e442543125bbf72b5
Alex Alexander <raregemalex@gmail.com>
2025-07-19 22:49:53 +0100

    Modified django-models/LibraryProject/relationship_app/urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,18 +1,14 @@
 # relationship_app/urls.py
-from django.urls import path  # This line was missing!
+from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
     path('register/', views.register_view, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-]
-
-urlpatterns = [
     path('books/add/', views.add_book, name='add_book'),
     path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
     path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
-    # You can add other views like 'book_list' as needed
 ]
