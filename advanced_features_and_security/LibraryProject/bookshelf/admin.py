74cdcf810c6ab33dcd3061332ac498cb9ed3e316
Alex Alexander <raregemalex@gmail.com>
2025-07-25 17:16:02 +0100

    Add Modified bookself/admin.py

---
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+admin.site.register(CustomUser, UserAdmin)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # shows these in admin list view
+    search_fields = ('title', 'author')                     # makes title and author searchable
+    list_filter = ('publication_year',)                     # adds a filter sidebar
+
+admin.site.register(Book, BookAdmin)
+
+# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ["username", "email", "date_of_birth", "is_staff"]
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
