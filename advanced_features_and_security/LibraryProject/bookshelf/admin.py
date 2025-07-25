925fc4a38280d5a2055c268895cb0ac8c8de0339
Alex Alexander <raregemalex@gmail.com>
2025-07-25 16:47:03 +0100

    Modified advanced_features_and_security/LibraryProject/bookshelf/admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -13,3 +13,14 @@ class BookAdmin(admin.ModelAdmin):
 admin.site.register(Book, BookAdmin)
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ["username", "email", "date_of_birth", "is_staff"]
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
