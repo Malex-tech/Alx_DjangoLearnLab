9f3c78213f0d52a8ff296194c33c49829c602573
Alex Alexander <raregemalex@gmail.com>
2025-08-01 22:44:59 +0100

    Add modified api_project files

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.generics import ListAPIView
 
-class BookList(generics.ListAPIView):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-# Create your views here.
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+    # Create your views here.
