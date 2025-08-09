86c994f3ad02f19f3774ca9e40993239dca063c0
Alex Alexander <raregemalex@gmail.com>
2025-08-09 23:35:19 +0100

    Add modified Advanced API project files

--- advanced-api-project/advanced_api_project/api/serializers.py
+++ advanced-api-project/advanced_api_project/api/serializers.py
@@ -10,7 +10,7 @@ class BookSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ['id', 'title', 'publication_year', 'author']  # Explicitly list fields
 
     def validate_publication_year(self, value):
         current_year = datetime.now().year
@@ -22,10 +22,40 @@ class BookSerializer(serializers.ModelSerializer):
 class AuthorSerializer(serializers.ModelSerializer):
     """
     Serializes the Author model with a nested list of their books.
-    Uses BookSerializer for the related 'books' field.
+    Books can also be created or updated through this serializer.
     """
-    books = BookSerializer(many=True, read_only=True)
+    books = BookSerializer(many=True)
 
     class Meta:
         model = Author
         fields = ['id', 'name', 'books']
+
+    def create(self, validated_data):
+        """
+        Create Author and nested Books.
+        """
+        books_data = validated_data.pop('books', [])
+        author = Author.objects.create(**validated_data)
+
+        for book_data in books_data:
+            Book.objects.create(author=author, **book_data)
+
+        return author
+
+    def update(self, instance, validated_data):
+        """
+        Update Author and nested Books.
+        If books data is provided, replace all existing books for this author.
+        """
+        books_data = validated_data.pop('books', None)
+
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+
+        if books_data is not None:
+            # Remove old books and add new ones
+            instance.books.all().delete()
+            for book_data in books_data:
+                Book.objects.create(author=instance, **book_data)
+
+        return instance
