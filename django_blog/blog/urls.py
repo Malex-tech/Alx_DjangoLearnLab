358c6fe5fabcb4a1181aadb8e387e4c66379d4a9
Alex Alexander <raregemalex@gmail.com>
2025-08-11 18:12:33 +0100

    Add modified Urls.py file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,3 +18,11 @@ urlpatterns = [
     path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
     path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/new/', views.post_create, name='post_create'),
+    path('post/<int:pk>/update/', views.post_update, name='post_update'),
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+]
