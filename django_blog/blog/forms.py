548b7992b0aff2e12c827a8b226767d815177361
Alex Alexander <raregemalex@gmail.com>
2025-08-15 13:20:24 +0100

    Add modified blog/form.py file

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -4,6 +4,15 @@ from django.contrib.auth.models import User
 from .models import Profile
 from .models import Post
 from .models import Comment
+from taggit.forms import TagWidget
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
