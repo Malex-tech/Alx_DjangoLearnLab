3140319413b503935cf56fddac43b8a017be442d
Alex Alexander <raregemalex@gmail.com>
2025-08-24 13:55:34 +0100

    Add custom user model and token-based authentication with register, login, and profile endpoints

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
 User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -19,6 +26,16 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
         Token.objects.create(user=user)
         return user
+    
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid credentials")
 
 
 class UserSerializer(serializers.ModelSerializer):
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -4,12 +4,32 @@ from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer, UserSerializer
+from rest_framework.views import APIView
+from .models import CustomUser
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+
 
 User = get_user_model()
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        user = CustomUser.objects.get(username=response.data['username'])
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'user': response.data, 'token': token.key})
+
+class LoginView(APIView):
+    permission_classes = []
+    
+    def post(self, request, *args, **kwargs):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'user': UserSerializer(user).data, 'token': token.key})
 
 class LoginView(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
